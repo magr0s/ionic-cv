73a5d5afe1c21557480c3c12a6bfa3fe4c49b186
Oleksandr Krokhmalnyi <magr0s.ak@gmail.com>
2021-01-18 23:17:45 +0200

    chore: Use Library component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,41 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-buttons slot="end">
+          <ion-button
+            fill="solid"
+            color="primary"
+            @click="openAddBookDialog"
+          >
+            Add Book
+          </ion-button>
+        </ion-buttons>
+
+        <ion-title>My library</ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <books-list />
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  modalController
+} from '@ionic/vue';
+
 import { defineComponent } from 'vue';
+
+import { BooksList, AddBookDialog } from '@/components/Library'
 
 export default defineComponent({
   name: 'Home',
@@ -32,15 +45,43 @@ export default defineComponent({
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonButtons,
+    IonButton,
+    BooksList
+  },
+  setup () {
+    const openAddBookDialog = async () => {
+      const modal = await modalController
+        .create({
+          component: AddBookDialog,
+          cssClass: 'modal--fullscreen',
+          componentProps: {
+            title: 'Create New Book'
+          }
+        });
+
+      modal.present();
+    }
+
+    return {
+      openAddBookDialog
+    }
   }
 });
 </script>
 
+<style lang="scss">
+ion-modal {
+  --width: 100%!important;
+  --height: 100%!important;
+}
+</style>
+
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -56,9 +97,9 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
